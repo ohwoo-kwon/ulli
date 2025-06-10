91b9604e69ddb15628e134afff4630a2724fcbd6
ohwoo-kwon <kwonow1@gmail.com>
2025-06-10 17:29:43 +0900

    hmall 에서는 nav 미노출

--- app/common/components/navigation.tsx
+++ app/common/components/navigation.tsx
@@ -14,51 +14,55 @@ export default function Navigation() {
   return (
     <header className="py-1 px-5 flex justify-between items-center bg-primary">
       <h1 className="text-primary-foreground text-xl font-bold tracking-widest">
-        <Link to="/">ULLI</Link>
+        {pathname !== "/hmall" ? <Link to="/">ULLI</Link> : "ULLI"}
       </h1>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              href="/images/upload"
-              className={cn(
-                navigationMenuTriggerStyle(),
-                "bg-primary text-primary-foreground",
-                pathname === "/images/upload"
-                  ? "bg-accent text-accent-foreground"
-                  : ""
-              )}
-            >
-              이미지 만들기
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              href="/images"
-              className={cn(
-                navigationMenuTriggerStyle(),
-                "bg-primary text-primary-foreground",
-                pathname === "/images" ? "bg-accent text-accent-foreground" : ""
-              )}
-            >
-              내 이미지
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              href="/products"
-              className={cn(
-                navigationMenuTriggerStyle(),
-                "bg-primary text-primary-foreground",
-                pathname === "/products"
-                  ? "bg-accent text-accent-foreground"
-                  : ""
-              )}
-            >
-              상품
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          {/* <NavigationMenuItem>
+      {pathname !== "/hmall" && (
+        <>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="/images/upload"
+                  className={cn(
+                    navigationMenuTriggerStyle(),
+                    "bg-primary text-primary-foreground",
+                    pathname === "/images/upload"
+                      ? "bg-accent text-accent-foreground"
+                      : ""
+                  )}
+                >
+                  이미지 만들기
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="/images"
+                  className={cn(
+                    navigationMenuTriggerStyle(),
+                    "bg-primary text-primary-foreground",
+                    pathname === "/images"
+                      ? "bg-accent text-accent-foreground"
+                      : ""
+                  )}
+                >
+                  내 이미지
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="/products"
+                  className={cn(
+                    navigationMenuTriggerStyle(),
+                    "bg-primary text-primary-foreground",
+                    pathname === "/products"
+                      ? "bg-accent text-accent-foreground"
+                      : ""
+                  )}
+                >
+                  상품
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              {/* <NavigationMenuItem>
             <NavigationMenuLink
               href="/advertisements"
               className={cn(
@@ -69,13 +73,15 @@ export default function Navigation() {
               광고
             </NavigationMenuLink>
           </NavigationMenuItem> */}
-        </NavigationMenuList>
-      </NavigationMenu>
-      <div>
-        <Button asChild variant="link" className="text-primary-foreground">
-          <Link to="/auth/login">로그인</Link>
-        </Button>
-      </div>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <div>
+            <Button asChild variant="link" className="text-primary-foreground">
+              <Link to="/auth/login">로그인</Link>
+            </Button>
+          </div>
+        </>
+      )}
     </header>
   );
 }
