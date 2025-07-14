513dc5762eec14c36fe3b547b56921c6948643a9
ohwoo-kwon <kwonow1@gmail.com>
2025-07-14 09:36:03 +0900

    replicate 변경

--- app/apis/image/image-generate.tsx
+++ app/apis/image/image-generate.tsx
@@ -23,72 +23,71 @@ export const action = async ({ request }: Route.ActionArgs) => {
     return data({ status: 401 }, { statusText: "잘못된 요청입니다." });
 
   const replicate = new Replicate();
-  const openai = new OpenAI({ apiKey: process.env.OPEN_AI_API_KEY });
+  // const openai = new OpenAI({ apiKey: process.env.OPEN_AI_API_KEY });
 
   const itemImgBuffer = await fileToBase64(validFormData.itemImg);
   const myImgBuffer = await fileToBase64(validFormData.myImg);
 
   // openai 를 통해 상품 이미지와 인물 이미지 해석
-  const [itemImgRes, myImgRes] = await Promise.all([
-    openai.responses.create({
-      model: "gpt-4.1-nano",
-      input: [
-        {
-          role: "user",
-          content: [
-            {
-              type: "input_text",
-              text: "Describe about the cloth. Output must be start with 'The cloth is' and just decribe about the cloth.",
-            },
-            {
-              type: "input_image",
-              image_url: `data:${validFormData.itemImg.type};base64,${itemImgBuffer}`,
-              detail: "high",
-            },
-          ],
-        },
-      ],
-    }),
-    openai.responses.create({
-      model: "gpt-4.1-nano",
-      input: [
-        {
-          role: "user",
-          content: [
-            {
-              type: "input_text",
-              text: "Describe about the person. Output must be start with 'The person is' and just decribe about the person and background.",
-            },
-            {
-              type: "input_image",
-              image_url: `data:${validFormData.myImg.type};base64,${myImgBuffer}`,
-              detail: "high",
-            },
-          ],
-        },
-      ],
-    }),
-  ]);
+  // const [itemImgRes, myImgRes] = await Promise.all([
+  //   openai.responses.create({
+  //     model: "gpt-4.1-nano",
+  //     input: [
+  //       {
+  //         role: "user",
+  //         content: [
+  //           {
+  //             type: "input_text",
+  //             text: "Describe about the cloth. Output must be start with 'The cloth is' and just decribe about the cloth.",
+  //           },
+  //           {
+  //             type: "input_image",
+  //             image_url: `data:${validFormData.itemImg.type};base64,${itemImgBuffer}`,
+  //             detail: "high",
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   }),
+  //   openai.responses.create({
+  //     model: "gpt-4.1-nano",
+  //     input: [
+  //       {
+  //         role: "user",
+  //         content: [
+  //           {
+  //             type: "input_text",
+  //             text: "Describe about the person. Output must be start with 'The person is' and just decribe about the person and background.",
+  //           },
+  //           {
+  //             type: "input_image",
+  //             image_url: `data:${validFormData.myImg.type};base64,${myImgBuffer}`,
+  //             detail: "high",
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   }),
+  // ]);
 
-  if (itemImgRes.error || myImgRes.error)
-    return data(
-      { error: itemImgRes.error?.message || myImgRes.error?.message },
-      { status: 400 }
-    );
+  // if (itemImgRes.error || myImgRes.error)
+  //   return data(
+  //     { error: itemImgRes.error?.message || myImgRes.error?.message },
+  //     { status: 400 }
+  // );
 
   // replicate 를 통해서 결과 이미지 생성
   const input = {
-    prompt: `Me: ${myImgRes.output_text}
-      Fitting cloth: ${itemImgRes.output_text}
-      Make me wears the Fitting cloth. I want to take off my cloth and change the cloth to the garment. Keep everything same in my image except my cloth. Change my cloth only.`,
+    prompt: `The @person wearing the @cloth. Keep @person pose and background.`,
     aspect_ratio: "3:4",
+    reference_tags: ["cloth", "person"],
     input_images: [
       `data:${validFormData.itemImg.type};base64,${itemImgBuffer}`,
       `data:${validFormData.myImg.type};base64,${myImgBuffer}`,
     ],
   };
 
-  const output = await replicate.run("flux-kontext-apps/multi-image-list", {
+  const output = await replicate.run("runwayml/gen4-image", {
     input,
   });
 
